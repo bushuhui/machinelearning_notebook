de2bf30b57aebe33395c5965c27eeefc847468bb
bushuhui <bushuhui@foxmail.com>
2020-01-07 09:59:29 +0800

    Move reference to code_cook

--- demo_code/3_CNN_MNIST.py
+++ demo_code/3_CNN_MNIST.py
@@ -42,7 +42,7 @@ class Net_CNN(nn.Module):
 
     def forward(self, x):
         x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
-        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
+        x = F.max_pool2d(F.relu(self.conv2(x)), (2, 2))
         x = x.view(x.size()[0], -1)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
